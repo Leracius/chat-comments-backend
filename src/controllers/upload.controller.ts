0638acaa4280545b403dbda3ba4707e4db9c1846
Axel Quintana <lucasaxelqg15@gmail@email.com>
2024-11-19 23:32:18 -0300

    installing missing dependeces

--- src/controllers/upload.controller.ts
+++ src/controllers/upload.controller.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from "express";
-import { handleUpload } from "../services/cloudinary";
 
 export const postImg = async (req: Request, res: Response) => {
   const file = req.body;
